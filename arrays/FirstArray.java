035c5dc675e8f5b7724774ad94dd63a6a3ad7900
kunal2bytes <kunal2bytes.solution@gmail.com>
2026-05-29 10:33:45 +0530

    bug fixed -130

--- arrays/FirstArray.java
+++ arrays/FirstArray.java
@@ -2,7 +2,8 @@ package arrays;
 
 public class FirstArray {
     public static void main(String[] args) {
-        int [] a= {101,102,103,104};
+  //bug 130
+        int [] a= {101,102,103,104,105,106,107,108,109,110};
         System.out.println(a.length);//4
         System.out.println(a[3]);//104
         for (int i=0; i<a.length;i++){
